01bc212275a2179b7edc6fa6d427052f147e1506
Jonathan Hong <jonhong777@gmail.com>
2018-06-27 16:09:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,8 +9,15 @@ class TicTacToe
     [6,7,8], # bottom row
     [0,3,6], # left column
     [1,4,7], # middle column
-    [2,5,8], #
-    [0,4,8],
-    [2,4,6]
+    [2,5,8], # right column
+    [0,4,8], # back diagonal
+    [2,4,6]  # forward diagonal
   ]
+  
+  def display_board()
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end 
